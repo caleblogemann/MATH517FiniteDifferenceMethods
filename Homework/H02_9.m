5e1d3f38c66589ca005fd04678ccf88dc521497b
Caleb Logemann <caleb.logemann@gmail.com>
2016-02-15 23:30:07 -0600

    Continued work on problems 8 and 9 with MATLAB code

--- Homework/H02_9.m
+++ Homework/H02_9.m
@@ -1,7 +1,9 @@
 f = @(x,y) -1.25*exp(x + .5*y);
 u = @(x,y) exp(x + .5*y);
-for N = 10*2.^(1:5)
-    [U, x, y] = Poisson2D(N, f, f);
-    uExact = ;
-    norm(U - uExact);
+Error = [];
+for N = 10*2.^(0:5)
+    [U, Ux, Uy] = Poisson2D(f, f, 1, N);
+    uExact = u(Ux, Uy);
+    Error = [Error, norm(U - uExact', 'inf')];
 end
+Error
--- Homework/Poisson2D.m
+++ Homework/Poisson2D.m
@@ -1,47 +1,88 @@
-function [U] = Poisson2D(N, f, g)
+function [U, Ux, Uy] = Poisson2D(f, g, L, N)
     p = inputParser;
-    p.addRequired('N', @Utils.isInteger);
     p.addRequired('f', @Utils.isFunctionHandle);
     p.addRequired('g', @Utils.isFunctionHandle);
-    p.parse(N, f, g);
+    p.addRequired('L', @(x) isnumeric(x) && x > 0);
+    p.addRequired('N', @Utils.isInteger);
+    p.parse(f, g, L, N);
 
-    h = 1/(N+1);
-    x = 0:h:1;
-    y = 0:h:1;
+    h = L/(N+1);
+    x = 0:h:L;
+    y = 0:h:L;
 
     % create functions to swap between row-wise ordering and i,j ordering
-    kFun = @(i, j) i + (j-1)*N;
-    iFun = @(k) mod(k,N);
-    jFun = @(k) floor(k/N) + 1;
+    %kFun = @(i, j) i + (j-1)*N;
+    iFun = @(k) mod(k-1,N)+1;
+    jFun = @(k) floor((k-1)/N) + 1;
+
+    k = 1:N^2;
+    % create vectors for x and y positions for each entry in U
+    Ux = x(iFun(k) + 1);
+    Uy = y(jFun(k) + 1);
 
     % vector of forcing function values at each index k
-    F = arrayfun(@(k) h^2*f(x(iFun(k) + 1), y(jFun(k) + 1)), 1:N^2);
+    F = h^2*f(Ux, Uy);
+    %arrayfun(@(k) h^2*f(x(iFun(k) + 1), y(jFun(k) + 1)), k);
     % add on boundary conditions to F
-    G = zeros(N^2, 1);
     % add bottom boundary
-    kBottom = find(jFun(1:N^2) == 1);
-    G(kBottom) = g(x(kBottom));
-
-
-    F = F + G;
+    kBottom = 1:N;
+    F(kBottom) = F(kBottom) + g(x(2:N+1),0);
+    % add top boundary
+    kTop = (N^2-N+1):N^2;
+    F(kTop) = F(kTop) + g(x(2:N+1), L);
+    % add left boundary
+    kLeft = 1:N:(N^2-N)+1;
+    F(kLeft) = F(kLeft) + g(0, y(2:N+1));
+    % add right boundary
+    kRight = N:N:N^2;
+    F(kRight) = F(kRight) + g(L, y(2:N+1));
 
     % build sparse matrix A
-    T = spdiags([-1, 4, 1], -1:1, N);
-    I = speye(N);
-    A = sparse(N^2, N^2);
-    for s = 1:N
-        % put T as diagonal block
-        A((s-1)*N+1:s*N,(s-1)*N+1:s*N) = T;
-        if(s > 1)
-            A((s-2)*N+1:(s-1)*N,(s-1)*N+1:s*N) = -I;
-        end
-        if(s < N)
-            A(s*N+1:(s+1)*N,(s-1)*N+1:s*N) = -I;
-        end
-    end
+    % build main diagonal
+    iMain = k';
+    jMain = k';
+    sMain = 4*ones(N^2,1);
+    % build upper main diagonal
+    iUpper = k';
+    iUpper(N:N:N^2) = [];
+    jUpper = k';
+    jUpper(1:N:N^2) = [];
+    sUpper = -ones(size(iUpper)); 
+    % build lower main diagonal
+    iLower = jUpper;
+    jLower = iUpper;
+    sLower = sUpper;
+    % build off upper diagonal
+    iU = (1:(N^2-N))';
+    jU = ((N+1):N^2)';
+    sU = -ones(size(iU));
+    % build off lower diagonal
+    iL = jU;
+    jL = iU;
+    sL = sU;
+    
+    iA = [iMain; iUpper; iLower; iU; iL];
+    jA = [jMain; jUpper; jLower; jU; jL];
+    sA = [sMain; sUpper; sLower; sU; sL];
+    A = sparse(iA, jA, sA);
+    %T = spdiags([-ones(N,1), 4*ones(N,1), -ones(N,1)], -1:1, N, N);
+    %I = speye(N);
+    %[iT, jT, sT] = find(T);
+    %
+    %A = sparse(N^2, N^2);
+    %for s = 1:N
+    %    % put T as diagonal block
+    %    A((s-1)*N+1:s*N,(s-1)*N+1:s*N) = T;
+    %    if(s > 1)
+    %        A((s-2)*N+1:(s-1)*N,(s-1)*N+1:s*N) = -I;
+    %    end
+    %    if(s < N)
+    %        A(s*N+1:(s+1)*N,(s-1)*N+1:s*N) = -I;
+    %    end
+    %end
 
-    U = A\F;
+    U = A\F';
 
     % change U from 1D vector to 2D matrix
-    U = vec2mat(U, N);
+    %U = vec2mat(U, N);
 end
